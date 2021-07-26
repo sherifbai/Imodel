be6443c1068f446c2d28915e7ea33b0ced8acccd
Sherif <godness980@gmail.com>
2021-07-26 17:48:03 +0500

    added order model

--- connections/mongodb.connection.js
+++ connections/mongodb.connection.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
-mongoose.connect(process.env.MONGODB_LINK)
+mongoose.connect(process.env.MONGODB_LINK);
 
-mongoose.connection.on('connected', () => console.log('MongoDB connected'))
-mongoose.connection.on('error', () => console.log('Failed to connect MongoDB'))
+mongoose.connection.on("connected", () => console.log("MongoDB connected"));
+mongoose.connection.on("error", () => console.log("Failed to connect MongoDB"));
 
-require('@models/user.model')
-require('@models/models.model')
+require("@models/user.model");
+require("@models/models.model");
+require("@models/order.model");
---
+++ models/order.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    district: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    deadline: {
+      type: Date,
+    },
+    budget: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["active", "completed", "moderate"],
+      default: "active",
+    },
+    takenBy: {
+      type: Schema.Types.ObjectId,
+      ref: "Model",
+    }
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
